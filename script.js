9b95cf8ce39bfea33a76cd4f07d0eb9558e0fd3a
parthrastogi04 <99319411+parthrastogi04@users.noreply.github.com>
2022-03-24 15:34:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+function myFunction1() {
+  var x = document.getElementById("overlay1");
+  x.style.display = "block";
+  document.getElementById("overlay1").style.display = "block";
+}
+
+function hide1() {
+  var x = document.getElementById("overlay1");
+  x.style.display = "none";
+  document.getElementById("overlay1").style.display = "none";
+}
+
+function myFunction2() {
+  var x = document.getElementById("overlay2");
+  x.style.display = "block";
+}
+
+function hide2() {
+  var x = document.getElementById("overlay2");
+  x.style.display = "none";
+}
+function myFunction3() {
+  var x = document.getElementById("overlay3");
+  x.style.display = "block";
+}
+
+function hide3() {
+  var x = document.getElementById("overlay3");
+  x.style.display = "none";
+}
+function myFunction4() {
+  var x = document.getElementById("overlay4");
+  x.style.display = "block";
+}
+
+function hide4() {
+  var x = document.getElementById("overlay4");
+  x.style.display = "none";
+}
+function myFunction5() {
+  var x = document.getElementById("overlay5");
+  x.style.display = "block";
+}
+
+function hide5() {
+  var x = document.getElementById("overlay5");
+  x.style.display = "none";
+}
+function myFunction6() {
+  var x = document.getElementById("overlay6");
+  x.style.display = "block";
+}
+
+function hide6() {
+  var x = document.getElementById("overlay6")
+  x.style.display = "none";
+}
+
+
+
+
